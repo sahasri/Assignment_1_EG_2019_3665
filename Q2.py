31dcf550ed7857d4411b3ba9c918bf6d7e75684e
sahasri <nimnasahasri123@gmail.com>
2024-03-14 09:02:13 +0530

    Make image gray scale

--- Q2.py
+++ Q2.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+# convert the image in to Grayscale
+original_Image = cv2.imread('Image1.jpg', cv2.IMREAD_GRAYSCALE)
 # convert the image in to RGB colors
-original_Image = cv2.cvtColor(cv2.imread('Image1.jpg'), cv2.COLOR_BGR2RGB)
+#original_Image = cv2.cvtColor(cv2.imread('Image1.jpg'), cv2.COLOR_BGR2RGB)
 
 def spatial_average(size,image):
     # Define the kernel for spatial averaging
@@ -20,13 +21,13 @@ spatial_ave_20x20= spatial_average(20,original_Image)
 
 
 fig, axs = plt.subplots(2, 2, figsize=(7, 12))
-axs[0,0].imshow(original_Image)
+axs[0,0].imshow(original_Image, cmap='gray')
 axs[0,0].set_title("Original Image\n")
-axs[0,1].imshow(spatial_ave_3x3)
+axs[0,1].imshow(spatial_ave_3x3, cmap='gray')
 axs[0,1].set_title("3X3 averaged Image\n")
-axs[1,0].imshow(spatial_ave_10x10)
+axs[1,0].imshow(spatial_ave_10x10, cmap='gray')
 axs[1,0].set_title("\n\n10X10 averaged Image\n")
-axs[1,1].imshow(spatial_ave_20x20)
+axs[1,1].imshow(spatial_ave_20x20, cmap='gray')
 axs[1,1].set_title("\n\n20X20 averaged Image\n")
 
 plt.show()
